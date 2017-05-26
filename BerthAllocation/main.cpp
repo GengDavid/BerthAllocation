6e4b3c1f1af6d6ee833d711e796b3c67033fe00a
GengDavid <825132036@qq.com>
2017-05-26 19:49:48 +0800

    modified greedy

--- BerthAllocation/main.cpp
+++ BerthAllocation/main.cpp
@@ -21,7 +21,9 @@ public:
   int arv_tm;
   int svc_tm;
   int berth_occupy;
-};
+}**ship;
+
+int n, m;
 
 struct Allocated {
   int leftx, lefty;
@@ -47,6 +49,48 @@ void judge(Allocated* allo,int allo_cnt, int lx, int ly, int rx, int ry, int &ov
   }
 }
 
+int evaluate(Allocated*& allo, int allo_cnt, int bad_allo) {
+  int ttwt = 0, last_departure_tm = -1;
+  for (int i = 0; i < allo_cnt; i++) {
+    ttwt += allo[i].lefty - ship[allo[i].id]->arv_tm;
+    if (allo[i].righty + 1 > last_departure_tm)last_departure_tm = allo[i].righty + 1;
+  }
+  cout << bad_allo << " " << ttwt << " " << last_departure_tm << endl;
+  return bad_allo * 100 + ttwt * 10 + last_departure_tm * 10;
+}
+
+void greedy(Allocated*& allo, int cnt, int& allo_cnt,int& bad_allo) {
+  for (int s = 0; s < cnt; s++) {
+    int flag = 0;
+    for (int tempy = ship[s]->arv_tm; tempy + ship[s]->svc_tm - 1 < m; tempy++) {
+      for (int i = 0; i + ship[s]->berth_occupy - 1 < n; i++) {
+        int lx = i; int ly = tempy;
+        int rx = lx + ship[s]->berth_occupy - 1;
+        int ry = ly + ship[s]->svc_tm - 1;
+        int overlap = 0;
+        if (lx == 6) {
+          int x = 0;
+        }
+        judge(allo, allo_cnt, lx, ly, rx, ry, overlap);
+        if (!overlap) {
+          allo[allo_cnt].leftx = lx;
+          allo[allo_cnt].lefty = ly;
+          allo[allo_cnt].rightx = rx;
+          allo[allo_cnt].righty = ry;
+          allo[allo_cnt].id = ship[s]->id;
+          allo_cnt++;
+          flag = 1;
+          break;
+        }
+      }
+      if (flag)break;
+    }//for tempy
+    if (!flag) {
+      bad_allo++;
+    }
+  }
+}
+
 int main() {
   ifstream bafile("C:\\Users\\Geng David\\Documents\\berth_list.txt");
   ofstream outfile("C:\\Users\\Geng David\\Documents\\result.txt", ofstream::app);
@@ -56,8 +100,8 @@ int main() {
   }
   while (getline(bafile, s)) {
     int com_flag = 0;
-    int n = 0, m = 0;
     int n_cnt = 0, m_cnt = 0;
+    n = 0;  m = 0;
     for (int i = 0; i < s.find_first_of(';'); i++) {
       if (is_num(s[i])) {
         if (!com_flag) {
@@ -76,7 +120,7 @@ int main() {
 
     int cnt = 0;
     int num0fship = (s.length() - s.find_first_of(';')) / 6;
-    Ship **ship = new Ship*[num0fship];
+    ship = new Ship*[num0fship];
     int buffer[3];
     for (int i = s.find_first_of(';'); i < s.length() - 1; i = s.find_first_of(';',i + 1)) {
       int bf_cnt = 0;
@@ -89,35 +133,10 @@ int main() {
     }
     Allocated* allo = new Allocated[cnt];
     int allo_cnt = 0, bad_allo = 0;
-    for (int s = 0; s < cnt; s++) {
-      int flag = 0;
-      for (int tempy = ship[s]->arv_tm; tempy + ship[s]->svc_tm - 1 < m; tempy++) {
-        for (int i = 0; i + ship[s]->berth_occupy - 1 < n; i++) {
-          int lx = i; int ly = tempy;
-          int rx = lx + ship[s]->berth_occupy - 1;
-          int ry = ly + ship[s]->svc_tm - 1;
-          int overlap = 0;
-          if (lx == 6) {
-            int x = 0;
-          }
-          judge(allo, allo_cnt,lx,ly,rx,ry,overlap);
-          if (!overlap) {
-            allo[allo_cnt].leftx = lx;
-            allo[allo_cnt].lefty = ly;
-            allo[allo_cnt].rightx = rx;
-            allo[allo_cnt].righty = ry;
-            allo[allo_cnt].id = ship[s]->id;
-            allo_cnt++;
-            flag = 1;
-            break;
-          }
-        }
-        if (flag)break;
-      }//for tempy
-      if (!flag) {
-        bad_allo++;
-      }
-    }
+    greedy(allo, cnt, allo_cnt, bad_allo);
+
+    int grade = evaluate(allo, allo_cnt, bad_allo);
+
     int out_cnt = 0;
     for (int i = 0; i < cnt; i++) {
       if (allo[out_cnt].id == i) {
@@ -133,13 +152,14 @@ int main() {
         outfile << s;
       }
     }
-    int ttwt = 0, last_departure_tm = -1;
-    for (int i = 0; i < allo_cnt; i++) {
-      ttwt += allo[i].lefty - ship[allo[i].id]->arv_tm;
-      if (allo[i].righty + 1 > last_departure_tm)last_departure_tm = allo[i].righty + 1;
-    }
     outfile << endl;
-    cout << bad_allo << " " << ttwt << " " << last_departure_tm << endl;
+
+    for (int i = 0; i < num0fship; i++) {
+      delete[] ship[i];
+    }
+    delete[] ship;
+    ship = nullptr;
+
   }
   bafile.close();
   outfile.close();
